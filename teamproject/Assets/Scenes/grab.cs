4bf7914ca9e600af470bc05955e2da39d08a21a7
yoonhachoe <48667973+yoonhachoe@users.noreply.github.com>
2019-12-05 15:12:06 +0900

    Team

---
+++ teamproject/Assets/Scenes/grab.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class grab : MonoBehaviour
+{
+    public GameObject waterword;
+    public GameObject myHand;
+    Vector3 waterwordPos;
+    Camera cam;
+    public float handPower;
+    //public static bool inHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        cam = GetComponentInChildren<Camera>();
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonDown("Fire1"))
+        {           
+            waterword.transform.SetParent(myHand.transform);
+            waterword.transform.localPosition = new Vector3(0, 0.8f, 0);
+            //inHand = true;
+        }
+
+
+    }
+}
---
+++ teamproject/Assets/Scenes/water.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour
+{
+	public GameObject waterword;
+    
+    void OnMouseDown(){
+    			Destroy(waterword);
+    }
+}
