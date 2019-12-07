3ff85ea20dd72008c3d819c74268cf4f5f790c9b
yoonhachoe <48667973+yoonhachoe@users.noreply.github.com>
2019-12-07 18:52:47 +0900

    jhg

--- teamproject/Assets/banana.cs
+++ teamproject/Assets/banana.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class banana : MonoBehaviour
 {
     public GameObject bananaword;
+    public TextMeshPro myText;
+    
     void OnMouseDown(){
-    	if(grab2.inHand)
+    	if(grab2.inHand1){
     		Destroy(bananaword);
+    		myText.text = "O";
+    	
+		}
     }
 }
