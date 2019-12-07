66b02ac071e73238f579b5af28c12f648812272c
mbnthp11 <58543396+mbnthp11@users.noreply.github.com>
2019-12-07 20:21:51 +0900

    ㅓㅏㅣ

--- teamproject/Assets/Scenes/banana.cs
+++ teamproject/Assets/Scenes/banana.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:teamproject/Assets/banana.cs
+
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,17 +18,3 @@ public class banana : MonoBehaviour
 		}
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class banana : MonoBehaviour
-{
-    public GameObject bananaword;
-    void OnMouseDown(){
-    	if(grab2.inHand)
-    		Destroy(bananaword);
-    }
-}
->>>>>>> fd5e8a68c0705c73089a4f6caee6b06f0ad4009e:teamproject/Assets/Scenes/banana.cs
--- teamproject/Assets/Scenes/grab2.cs
+++ teamproject/Assets/Scenes/grab2.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:teamproject/Assets/grab2.cs
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -85,35 +84,3 @@ public class grab2 : MonoBehaviour
 
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class grab2 : MonoBehaviour
-{
-	public GameObject bananaword;
-    public GameObject myHand;
-    Vector3 bananawordPos;
-    Camera cam;
-    public float handPower;
-    public static bool inHand;
-    // Start is called before the first frame update
-    void Start()
-    {
-        cam = GetComponentInChildren<Camera>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {           
-            bananaword.transform.SetParent(myHand.transform);
-            bananaword.transform.localPosition = new Vector3(0.8f, 0.8f, 0);
-            inHand = true;
-        }
-    }
-}
->>>>>>> fd5e8a68c0705c73089a4f6caee6b06f0ad4009e:teamproject/Assets/Scenes/grab2.cs
