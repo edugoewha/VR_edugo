8ccc76dccdc944a4573561dc7f6dd4f4bb2d418e
yoonhachoe <48667973+yoonhachoe@users.noreply.github.com>
2019-12-08 14:11:14 +0900

    asdasd

--- teamproject/Assets/Scenes/banana.cs
+++ teamproject/Assets/Scenes/banana.cs
@@ -1,21 +1,51 @@
-
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class banana : MonoBehaviour
 {
-    public GameObject bananaword;
-    public TextMeshPro myText;
+	private AudioSource Oaudio;
+    private AudioSource Xaudio;
+    public AudioClip OSound;
+    public AudioClip XSound;
+    public GameObject Bananaword;
+    
+    void Start()
+    {
+        this.Oaudio = this.gameObject.AddComponent<AudioSource>();
+        this.Oaudio.clip = this.OSound;
+        this.Oaudio.loop = false;
+
+        this.Xaudio = this.gameObject.AddComponent<AudioSource>();
+        this.Xaudio.clip = this.XSound;
+        this.Xaudio.loop = false;
+
+    }
     
     void OnMouseDown(){
-    	if(grab2.inHand1){
-    		Destroy(bananaword);
-    		myText.text = "O";
-    	
-		}
+    	if (grab2.inHand1)
+        {
+            grab2.newText[1].enabled = true;
+            grab2.inHand1 = false;
+            Destroy(Bananaword);
+            grab2.newText[1].text = "O";
+            this.Oaudio.Play();
+            Invoke("DisableText", 1f);
+           
+            grab2.count--;
+        }
+        if (grab2.inHand6 || grab2.inHand2 || grab2.inHand3 || grab2.inHand4 || grab2.inHand5)
+        {
+            grab2.newText[1].enabled = true;
+            grab2.newText[1].text = "X";
+            this.Xaudio.Play();
+            Invoke("DisableText", 1f);
+         
+        }
+    }
+    void DisableText()
+    {
+        grab2.newText[1].enabled = false;
     }
 
 }
--- teamproject/Assets/Scenes/grab2.cs
+++ teamproject/Assets/Scenes/grab2.cs
@@ -1,17 +1,28 @@
-
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class grab2 : MonoBehaviour
 {
+	private AudioSource audio;
+    public AudioClip jumpSound;
+
 	public GameObject bananaword;
     public GameObject tigerword;
     public GameObject spoonword;
     public GameObject carword;
     public GameObject fishword;
     public GameObject elephantword;
+    public GameObject elephantobject;
+    public GameObject Board;
+    public GameObject Plane1;
+    public GameObject Plane2;
+    public GameObject Plane3;
+    public GameObject Plane4;
+    public GameObject Plane5;
+    public GameObject Plane6;
     public GameObject myHand;
     Vector3 bananawordPos;
     Vector3 tigerwordPos;
@@ -30,15 +41,51 @@ public class grab2 : MonoBehaviour
 
     Ray ray;
     RaycastHit hit;
+    public static Text[] newText;
+    public static int count;
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponentInChildren<Camera>();
+        newText = GetComponentsInChildren<Text>();
+        
+        count = 6;
+        elephantobject.SetActive(false);
+
+        this.audio = this.gameObject.AddComponent<AudioSource>();
+        this.audio.clip = this.jumpSound;
+        this.audio.loop = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+		if (count == 0)
+        {
+            Invoke("DisableText", 1f);
+            this.audio.Play();
+            newText[0].text = "배고픈 코끼리에게 과일을 가져다 주세요";
+            Destroy(Board);
+            Destroy(Plane1);
+            Destroy(Plane2);
+            Destroy(Plane3);
+            Destroy(Plane4);
+            Destroy(Plane5);
+            Destroy(Plane6);
+            elephantobject.SetActive(true);
+            count--;
+            //Board.SetActive(false);
+            //Plane1.SetActive(false);
+            //Plane2.SetActive(false);
+            //Plane3.SetActive(false);
+            //Plane4.SetActive(false);
+            //Plane5.SetActive(false);
+            //Plane6.SetActive(false);
+        }
+        else if(count > 0)
+        {
+            newText[0].text = "물건을 집어서 맞는 단어를 누르세요";
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -83,6 +130,10 @@ public class grab2 : MonoBehaviour
             
         }
 
+    }
+    void DisableText()
+    {
+        newText[1].enabled = false;
     }
 }
 
